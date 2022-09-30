1a1005c7dabeaa7e5dea3547349ae5f51e4a174a
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2022-09-29 22:34:30 -0500

    URL Rompecabezas
    Se genera Url para los rompecabezas ya jugables

--- Rompecabezas_route.py
+++ Rompecabezas_route.py
@@ -1,7 +1,9 @@
 import controller.partPuzzleController as partPuzzleCm
 import controller.pinturaController as pinturaC
+import controller.pintorController as pintorC
 from flask import Blueprint,render_template
 import random
+import numpy as np
 
 Rompecabezas_Blueprint = Blueprint('Rompecabezas',__name__,template_folder='templates', static_folder='static')
 
@@ -9,11 +11,21 @@ Rompecabezas_Blueprint = Blueprint('Rompecabezas',__name__,template_folder='temp
 def pintor():
     return "Pagina Questions"
 
-@Rompecabezas_Blueprint.route('/pruebas_locas')
+@Rompecabezas_Blueprint.route('/Rompecabezas_play')
 def pruebas_locas():
     id = random.randint(1, 2)
     Rompecabezas = partPuzzleCm.obtenerPartesPintura(id)
     oneArt = pinturaC.obtener_unico_pintura(id)
+    presentacion=pinturaC.presentarPintura(id)
     ruta= oneArt[8]
-    #ruta=" url_for('static', filename= '"+str(ruta)+"')"
-    return render_template('pruebas_locas.html',Rompecabezas=Rompecabezas,ruta=ruta)
+    partes=np.sqrt(presentacion[2])
+    print(str(presentacion))
+    
+    context={
+        'Rompecabezas':Rompecabezas,
+        'ruta':ruta,
+        'partes':partes,
+        'Presentacion':presentacion,
+        'Datos':oneArt
+    }
+    return render_template('Rompecabezas.html',**context)
