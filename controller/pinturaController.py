6993611c5f44f667e8578809d781e351ef15a839
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2022-09-29 22:33:47 -0500

    Presentacion pintura
    Se genera presentacion de la pintura obteniendo datos espcificos desde la base de datos

--- controller/pinturaController.py
+++ controller/pinturaController.py
@@ -35,6 +35,23 @@ def listar_pinturas():
     conexion.close()
     return pinturas
 
+def presentarPintura(id):
+    conexion = obtener_conexion()
+    pinturas = []
+    with conexion.cursor() as cursor:
+        cursor.execute(f"""SELECT p.idPintura , p.Nombre_pintura ,p.numero_piezas  ,
+                CONCAT( a.Nombre_autor,' ',a.Apellido) as Nombre_autor ,p.year_elaboracion ,ep.Nombre_estilo,c.Nombre_continente 
+                FROM  bosdos6qw6vefrichu88.Pintura p 
+                inner join bosdos6qw6vefrichu88.Autor a on(p.Autor_idAutor=a.idAutor)
+                inner join bosdos6qw6vefrichu88.Estilos_pintura ep on (p.Estilos_pintura_idEstilos_pintura=ep.idEstilos_pintura)
+                inner JOIN bosdos6qw6vefrichu88.Continente c on(p.Continente_idContinente=c.idContinente)
+                where p.idPintura ={id}
+                order by p.idPintura asc""")
+        pinturas = cursor.fetchone()
+    conexion.close()
+    return pinturas
+
+
 def obtener_unico_pintura(id):
     conexion = obtener_conexion()
     pintura = []
