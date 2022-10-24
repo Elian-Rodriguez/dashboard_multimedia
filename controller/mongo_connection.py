ac7c9f84206b4ab832b5b009d9e611a0931ebbf1
Elian Eduardo Rodriguez Benitez <elian.rodriguez@campusucc.edu.co>
2022-10-23 22:01:04 -0500

    Almacenamiento de respuestas
    Se agrega la estructura de almacenamiento de los datos segun las respuestas de los niños

--- controller/mongo_connection.py
+++ controller/mongo_connection.py
@@ -4,6 +4,7 @@ import pymongo
 def obtenerConexionMongo():
     client = pymongo.MongoClient("mongodb+srv://alien_83:Arelis11@cluster0.4ur0oyq.mongodb.net/?retryWrites=true&w=majority")
     db = client.test
+    print(db)
     return client
 def obtenerDataBase():
     client =  obtenerConexionMongo()
@@ -17,13 +18,17 @@ def insertDocument(collection, data):
     """
     return collection.insert_one(data).inserted_id
 
-def insertNote(name:str,idPregunta,idRespuesta):
+def insertNote(name:str,grado,idPregunta,idRespuesta,puntaje):
     name = name.upper()
     idPregunta=int(idPregunta)
     idRespuesta=int(idRespuesta)
     new_show = {
     "name": name,
-    "year": 1994
+    "grado":grado,
+    "idPregunta": idPregunta,
+    "idRespuesta":idRespuesta,
+    "Puntaje":puntaje
     }
     return (insertDocument(obtenerDataBase(), new_show))
-    
+
+obtenerConexionMongo()
